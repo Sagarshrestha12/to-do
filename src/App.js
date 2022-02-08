6c1d59075d3c5380cd6389ba410ea77b1ee719dd
Sagarshrestha12 <sagarshrestha4339@gmail.com>
2022-02-08 12:53:40 +0545

    Added the input text field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-// import logo from './logo.svg';
 import "./App.css";
 import Header from "./components/Header";
 import ToDoList from "./components/ToDoList";
+import InputWork from "./components/InputWork";
+
 function App() {
   return (
     <div>
       <Header name="TO-DO" />
+      <InputWork />
       <ToDoList task="Add some lemon" />
       <ToDoList task="Add some sugar" />
     </div>
--- src/components/InputWork.js
+++ src/components/InputWork.js
@@ -1,3 +1,15 @@
 function InputWork() {
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <label for="work">Enter Text</label>
+        <input type="text" id="work" name="do" required></input>
+        <button type="submit" value="">
+          Add
+        </button>
+      </form>
+    </div>
+  );
 }
+
+export default InputWork;
