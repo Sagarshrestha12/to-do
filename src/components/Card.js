f0714c96b66c7b21b115ea51cbb913c25fa6bc49
Sagarshrestha12 <sagarshrestha4339@gmail.com>
2022-02-08 19:31:06 +0545

    Added event Handler on input text

---
+++ src/components/Card.js
@@ -0,0 +1,4 @@
+function Card(props) {
+  return <div className={props.className}>{props.children}</div>;
+}
+export default Card;
--- src/components/InputWork.js
+++ src/components/InputWork.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 function InputWork() {
+  let [display, setDisplay] = useState("");
+  const [status, setStatus] = useState("Enter value");
+
+  const handleAddBtn = (event) => {
+    event.preventDefault();
+    setStatus(display);
+    // console.log(display);
+    setDisplay("");
+  };
+
+  const handleToDo = (event) => {
+    // display = event.target.value;
+    setDisplay(event.target.value);
+  };
+
   return (
     <div>
-      <form>
-        <label for="work">Enter Text</label>
-        <input type="text" id="work" name="do" required></input>
+      <form onSubmit={handleAddBtn}>
+        <label htmlFor="work">{status}</label>
+        <input
+          type="text"
+          id="work"
+          name="do"
+          value={display}
+          onChange={handleToDo}
+          required
+        ></input>
         <button type="submit" value="">
           Add
         </button>
